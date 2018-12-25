01d16e1d460dcd2ddecb35b58f6fd633b93be52f
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-25 17:41:43 +0700

    Create tembak.sh

---
+++ tembak.sh
@@ -0,0 +1,54 @@
+figlet MR_XID  | lolcat
+
+echo                    " [======<>======<>======<>======]"|lolcat
+echo                    " [Author   :  MR_XiD            ]"|lolcat
+echo                    " [Team     : JATIM BLACKHAT     ]"|lolcat
+echo                    " [Note     : Jangan reqode ya   ]"|lolcat
+echo                    " [Thansk to: All member         ]"|lolcat
+echo                    " [thanks to: I T A              ]"|lolcat
+echo                    " [Note2    : Copyright©MR_XID   ]"|lolcat
+echo                    " [======<>======<>======<>======]"|lolcat
+
+lagi=1
+while [ $lagi -lt 7 ];
+do
+echo -e""
+echo -e $b "1.  TEMBAK CEWE${enda}";
+echo -e"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" | lolcat
+echo -e $r "2.  MENEMUKAN HATI WEBSITE ${endc}";
+echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" | lolcat
+echo -e "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" saf;
+
+# TEMBAK CEWE
+case $saf in
+(1)
+echo -e "----------------------"|lolcat
+echo -e $r "1.cara tembak cewe ${endc}";
+echo -e "----------------------"|lolcat
+echo -e "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" saf;
+
+case $saf in
+(1)echo "CARA MENEMBAK CEWEK YA TONG 😅" | lolcat
+echo "PERTAMA KAMU HARUS PUNYA DUIT"
+echo "KALO KAGA YAUDAH GAGAL DEH 😅😅
+echo "JOMBLO HAPPY AJA TOD 😂
+exit
+
+;;
+
+#Install  admin-finder
+
+2) git clone https://github.com/the-c0d3r/admin-finder.git
+echo -e "${y} cara menggunakan admin finder"
+echo -e "${y} cd admin-finder"
+echo -e "${y} python admin-finder.py"
+echo
+
+;;
+
+*) echo "Tod nomor yang elu pilih kaga ada. coky dulu ya"
+esac
+done
+done
